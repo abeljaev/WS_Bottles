321011817a52e3f1b2ecfbc5b22f4e7cbe0299a1
A.Belyaev <A.belyaev@skoltech.ru>
2025-12-22 12:46:39 +0300

    Small change in cfg

--- core/config.py
+++ core/config.py
@@ -42,7 +42,7 @@ class Settings:
     """Настройки inference сервиса."""
 
     # Модель
-    model_path: Path = field(default_factory=lambda: Path("/home/radxa/Documents/CODE/BottleClassifier/weights/best_11s_rknn_model"))
+    model_path: Path = field(default_factory=lambda: Path("weights/best_11s_rknn_model"))
     image_size: int = 1280
     warmup_runs: int = 2
 
@@ -92,10 +92,7 @@ class Settings:
 
         return cls(
             # Модель
-            model_path=_get_env_path(
-                "MODEL_PATH",
-                "/home/radxa/Documents/CODE/BottleClassifier/weights/best_11s_rknn_model"
-            ),
+            model_path=_get_env_path("MODEL_PATH", "weights/best_11s_rknn_model"),
             image_size=_get_env_int("IMAGE_SIZE", 1280),
             warmup_runs=_get_env_int("WARMUP_RUNS", 2),
 
